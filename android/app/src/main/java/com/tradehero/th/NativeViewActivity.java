391f3ef7a02e65d117c734c5651f1e76c424d996
Yu Zheng(Sam) <sam@tradehero.mobi>
2016-03-17 16:34:39 +0800

    Google App Indexing API.

--- android/app/src/main/java/com/tradehero/th/NativeViewActivity.java
+++ android/app/src/main/java/com/tradehero/th/NativeViewActivity.java
@@ -1,10 +1,15 @@
 package com.tradehero.th;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import butterknife.ButterKnife;
 
@@ -14,6 +19,11 @@ import butterknife.ButterKnife;
 public class NativeViewActivity extends AppCompatActivity {
 
     public static final String START_FRAGMENT_NAME = "fragmentName";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +47,13 @@ public class NativeViewActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.th_toolbar);
         this.setSupportActionBar(toolbar);
         setupToolbar(toolbar, "我的自选", "完成");
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    public void setHeadLeftCallback() {
+
     }
 
     private void setupToolbar(Toolbar toolbar, String title, String leftText) {
@@ -45,5 +62,45 @@ public class NativeViewActivity extends AppCompatActivity {
 
         tvHeadLeft.setText(leftText);
         tvHeadMiddleMain.setText(title);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "NativeView Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.tradehero.th/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "NativeView Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.tradehero.th/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }

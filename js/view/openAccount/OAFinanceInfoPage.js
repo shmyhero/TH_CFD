5299be317cb7b24f64850b0a9324045b8d0d3f5a
nuqiewuoz <william@w159582.dow.com>
2016-06-23 12:19:10 +0800

    update personal info.

--- js/view/openAccount/OAFinanceInfoPage.js
+++ js/view/openAccount/OAFinanceInfoPage.js
@@ -5,13 +5,14 @@ import React from 'react';
 import {
 	StyleSheet,
 	View,
-	Platform,
+	Dimensions,
 } from 'react-native';
 
 var Button = require('../component/Button')
 var MainPage = require('../MainPage')
 var ColorConstants = require('../../ColorConstants')
 
+var {height, width} = Dimensions.get('window')
 
 var OAFinanceInfoPage = React.createClass({
 
@@ -26,12 +27,14 @@ var OAFinanceInfoPage = React.createClass({
 	render: function() {
 		return (
 			<View style={styles.wrapper}>
-				<Button style={styles.buttonArea}
-					enabled={true}
-					onPress={this.gotoNext}
-					textContainerStyle={styles.buttonView}
-					textStyle={styles.buttonText}
-					text='下一步' />
+				<View style={styles.bottomArea}>
+					<Button style={styles.buttonArea}
+						enabled={true}
+						onPress={this.gotoNext}
+						textContainerStyle={styles.buttonView}
+						textStyle={styles.buttonText}
+						text='下一步' />
+				</View>
 			</View>
 		);
 	},
@@ -40,8 +43,34 @@ var OAFinanceInfoPage = React.createClass({
 var styles = StyleSheet.create({
 	wrapper: {
 		flex: 1,
-		alignItems: 'stretch',
+   		alignItems: 'stretch',
+    	justifyContent: 'space-around',
 		backgroundColor: ColorConstants.BACKGROUND_GREY,
+	},
+	
+	bottomArea: {
+		height: 72, 
+		backgroundColor: 'white',
+		alignItems: 'flex-end',
+		flexDirection:'row'
+	},
+	buttonArea: {
+		backgroundColor: 'green',
+		width: width*0.92,
+		marginLeft: width*0.04,
+		marginBottom: 16,
+		borderRadius: 3,
+	},
+	buttonView: {
+		height: 40,
+		borderRadius: 3,
+		backgroundColor: '#4567a4',
+		justifyContent: 'center',
+	},
+	buttonText: {
+		fontSize: 17,
+		textAlign: 'center',
+		color: '#ffffff',
 	},
 });
 
--- js/view/openAccount/OAPersonalInfoPage.js
+++ js/view/openAccount/OAPersonalInfoPage.js
@@ -4,32 +4,79 @@ import React from 'react';
 import {
 	StyleSheet,
 	View,
+	Dimensions,
+	ListView,
+	Text,
+	TextInput,
 } from 'react-native';
 
 var Button = require('../component/Button')
 var MainPage = require('../MainPage')
 var ColorConstants = require('../../ColorConstants')
 
+var {height, width} = Dimensions.get('window')
+var rowPadding = Math.round(18*width/375)
+var fontSize = Math.round(16*width/375)
+var listRawData = [
+		{"key":"姓", "value":""},
+		{"key":"名", "value":""},
+		{"key":"性别", "value":""},
+		{"key":"出生日期", "value":""},
+		{"key":"民族", "value":""},
+		{"key":"身份证号", "value":""},
+		{"key":"证件地址", "value":""},
+		{"key":"签发机关", "value":""},
+		{"key":"有效期限", "value":""}];
 
 var OAPersonalInfoPage = React.createClass({
+	getInitialState: function() {
+		var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+		return {
+			personalInfo: listRawData,
+			dataSource: ds.cloneWithRows(listRawData),
+		};
+	},
 
 	gotoNext: function() {
 		//TODO, check
 		this.props.navigator.push({
 			name: MainPage.OPEN_ACCOUNT_ROUTE,
-			step: 2,
+			step: 3,
 		});
+	},
+
+	renderRow: function(rowData, sectionID, rowID) {
+		return (
+			<View style={styles.rowWrapper}>
+				<Text style={styles.rowTitle}>{rowData.key}</Text>
+			</View>
+			)
+	},
+
+	renderSeparator: function(sectionID, rowID, adjacentRowHighlighted){
+		return (
+			<View style={styles.line} key={rowID}>
+				<View style={styles.separator}/>
+			</View>
+			)
 	},
 
 	render: function() {
 		return (
 			<View style={styles.wrapper}>
-				<Button style={styles.buttonArea}
-					enabled={true}
-					onPress={this.gotoNext}
-					textContainerStyle={styles.buttonView}
-					textStyle={styles.buttonText}
-					text='下一步' />
+			    <ListView
+			    	style={styles.list}
+					dataSource={this.state.dataSource}
+					renderRow={this.renderRow}
+					renderSeparator={this.renderSeparator} />
+				<View style={styles.bottomArea}>
+					<Button style={styles.buttonArea}
+						enabled={true}
+						onPress={this.gotoNext}
+						textContainerStyle={styles.buttonView}
+						textStyle={styles.buttonText}
+						text='下一步' />
+				</View>
 			</View>
 		);
 	},
@@ -38,8 +85,61 @@ var OAPersonalInfoPage = React.createClass({
 var styles = StyleSheet.create({
 	wrapper: {
 		flex: 1,
-		alignItems: 'stretch',
+   		alignItems: 'stretch',
+    	justifyContent: 'space-around',
 		backgroundColor: ColorConstants.BACKGROUND_GREY,
+	},
+
+	list: {
+		flex: 1,
+	},
+	rowWrapper: {
+		flexDirection: 'row',
+		alignSelf: 'stretch',
+		alignItems: 'center',
+		paddingLeft: 15,
+		paddingRight: 15,
+		paddingBottom: rowPadding,
+		paddingTop: rowPadding,
+		backgroundColor: '#ffffff',
+	},
+	line: {
+		height: 0.5,
+		backgroundColor: 'white',
+	},
+	separator: {
+		marginLeft: 0,
+		height: 0.5,
+		backgroundColor: '#ececec',
+	},
+	rowTitle:{
+		fontSize: fontSize,
+		color: '#333333'
+	},
+
+	bottomArea: {
+		height: 72, 
+		backgroundColor: 'white',
+		alignItems: 'flex-end',
+		flexDirection:'row'
+	},
+	buttonArea: {
+		backgroundColor: 'green',
+		width: width*0.92,
+		marginLeft: width*0.04,
+		marginBottom: 16,
+		borderRadius: 3,
+	},
+	buttonView: {
+		height: 40,
+		borderRadius: 3,
+		backgroundColor: '#4567a4',
+		justifyContent: 'center',
+	},
+	buttonText: {
+		fontSize: 17,
+		textAlign: 'center',
+		color: '#ffffff',
 	},
 });
 

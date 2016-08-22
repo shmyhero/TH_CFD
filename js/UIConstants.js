869b2dd906c1c2e331d948208f865c808caf2bc5
Yu Zheng(Sam) <sam@tradehero.mobi>
2016-08-22 10:36:45 +0800

    Update the header height.

--- js/UIConstants.js
+++ js/UIConstants.js
@@ -5,7 +5,7 @@ import {
 } from 'react-native';
 
 export let TAB_BAR_HEIGHT = 50
-export let HEADER_HEIGHT = 64
+export let HEADER_HEIGHT = Platform.OS === 'android' ? 48 : 64
 export let SCROLL_TAB_HEIGHT = 48
 export let ANDROID_LIST_VIEW_HEIGHT_MAGIC_NUMBER = Platform.OS === 'android' ? 28 : 0
 

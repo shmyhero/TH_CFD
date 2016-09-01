1b0d15c1ca7214a4640bfae6112f5e4b40d26912
Neko <radishjr@gmail.com>
2016-09-01 14:43:33 +0800

    Add web socket price listener in edit alert page. Fix some bugs.

--- js/view/EditAlertPage.js
+++ js/view/EditAlertPage.js
@@ -17,77 +17,116 @@ var NavBar = require('./NavBar')
 var NetConstants = require('../NetConstants')
 var LogicData = require('../LogicData')
 var WebSocketModule = require('../module/WebSocketModule')
-
+var UIConstants = require('../UIConstants')
 var {height, width} = Dimensions.get('window')
 
 var EditAlertPage = React.createClass({
 
 	propTypes: {
-		stockName: React.PropTypes.string,
 		stockId:React.PropTypes.number,
-		stockPrice: React.PropTypes.number,
-		stockPriceAsk: React.PropTypes.oneOfType([React.PropTypes.number, React.PropTypes.string]),
-		stockPriceBid: React.PropTypes.oneOfType([React.PropTypes.number, React.PropTypes.string]),
-		currentUp: React.PropTypes.oneOfType([React.PropTypes.number, React.PropTypes.string]),
-		currentDwon: React.PropTypes.oneOfType([React.PropTypes.number, React.PropTypes.string]),
-		switchHigh: React.PropTypes.bool,
-		switchLow: React.PropTypes.bool,
 		stockInfo: React.PropTypes.object,
 		stockAlert: React.PropTypes.object,
 	},
 
 	getDefaultProps() {
 		return {
-			currentUp: 0,
-			currentDwon: 0,
-			switchHigh: false,
-			switchLow: false,
+		
 		}
 	},
 
 	getInitialState: function() {
 		return {
-			currentUp: this.props.currentUp,
-			currentDwon: this.props.currentDwon,
-			switchHigh: this.props.switchHigh,
-			switchLow: this.props.switchLow,
-			stockInfo: {},
+			HighPrice: 0,
+			LowPrice: 0,
+			HighEnabled: false,
+			LowEnabled: false,
+			stockInfo: this.props.stockInfo,
 			stockPrice: 0,	//TODO: use real price
+			upError: null,
+			downError: null,
 			stockPriceAsk: 0,
 			stockPriceBid: 0,
 		};
 	},
 
 	componentDidMount: function(){
+		console.log("test data")
+		console.log(this.props.stockInfo)
+		console.log(this.props.stockAlert)
+
+		if(this.props.stockAlert){
+			this.setState({
+				HighPrice: this.props.stockAlert.HighPrice,
+				LowPrice: this.props.stockAlert.LowPrice,
+				HighEnabled: this.props.stockAlert.HighEnabled,
+				LowEnabled: this.props.stockAlert.LowEnabled
+			});
+		}
+
+		this.loadStockInfo();
+	},
+
+	loadStockInfo: function() {
+		var url = NetConstants.GET_STOCK_DETAIL_API
+		url = url.replace(/<stockCode>/, this.props.stockInfo.id)
+
+		NetworkModule.fetchTHUrl(
+			url,
+			{
+				method: 'GET',
+				showLoading: true,
+			},
+			(responseJson) => {
+				/*
+				var currencySymbol = responseJson.ccy
+				if (currencySymbol != UIConstants.USD_CURRENCY) {
+					// Find the USD/ccy fx data first.
+					var fxData = LogicData.getFxDataBySymbol(UIConstants.USD_CURRENCY + currencySymbol)
+					if (!fxData) {
+						fxData = LogicData.getFxDataBySymbol(currencySymbol + UIConstants.USD_CURRENCY)
+					}
+					if (fxData) {
+						var fxId = fxData.id
+						responseJson.fxData = fxData
+						var previousInterestedStocks = WebSocketModule.getPreviousInterestedStocks()
+						previousInterestedStocks += ',' + fxId
+						WebSocketModule.registerInterestedStocks(previousInterestedStocks)
+					}
+				}
+				*/
+
+				this.setState({
+					stockInfo: responseJson,
+					stockPriceBid: responseJson.bid,
+					stockPriceAsk: responseJson.ask,
+				})
+
+				this.connectWebSocket()
+			},
+			(errorMessage) => {
+				Alert.alert('', errorMessage);
+			}
+		)
+	},
+
+	connectWebSocket: function() {
 		WebSocketModule.registerCallbacks(
 			(realtimeStockInfo) => {
-				console.log("update stock info: " + JSON.stringify(realtimeStockInfo))
+				console.log("websocket changes! " + JSON.stringify(realtimeStockInfo))
 				for (var i = 0; i < realtimeStockInfo.length; i++) {
-					if (this.props.stockCode == realtimeStockInfo[i].id ) {
-						if (this.state.chartType === NetConstants.PARAMETER_CHARTTYPE_TEN_MINUTE
-							 && this.state.stockInfo != undefined
-							 && this.state.stockInfo.priceData != undefined) {
-							var stockinfo = this.state.stockInfo
-							var price = realtimeStockInfo[i].last
-							stockinfo.priceData.push({"p":price,"time":realtimeStockInfo[i].time})
-							this.setState({
-								stockInfo: stockinfo,
-								stockPrice: realtimeStockInfo[i].last,
-								stockPriceAsk: realtimeStockInfo[i].ask,
-								stockPriceBid: realtimeStockInfo[i].bid,
-							})
-						}
-						else if(this.state.stockPrice !== realtimeStockInfo[i].last) {
-							this.setState({
-								stockPrice: realtimeStockInfo[i].last,
-								stockPriceAsk: realtimeStockInfo[i].ask,
-								stockPriceBid: realtimeStockInfo[i].bid,
-							})
-						}
+					if (this.props.stockInfo.id == realtimeStockInfo[i].id ) {
+						this.setState({
+							stockPrice: realtimeStockInfo[i].last,
+							stockPriceAsk: realtimeStockInfo[i].ask,
+							stockPriceBid: realtimeStockInfo[i].bid,
+						})
+
+						this.validateInputData();
 						break;
 					}
 				};
 
+				/*
 				if (this.state.stockInfo.fxData) {
 					var fxId = this.state.stockInfo.fxData.id
 					for (var i = 0; i < realtimeStockInfo.length; i++) {
@@ -100,8 +139,10 @@ var EditAlertPage = React.createClass({
 						}
 					};
 				}
+				*/
 			})
 	},
+
 
 	componentWillUnmount: function(){
 		WebSocketModule.cleanRegisteredCallbacks();
@@ -115,13 +156,25 @@ var EditAlertPage = React.createClass({
 		this.props.navigator.pop()
 	},
 
+	validateInputData: function(){
+		this.updatePrice(1, this.state.HighPrice)
+		this.updatePrice(2, this.state.LowPrice)
+	},
+
 	updatePrice: function(type, text){
+		var value = parseFloat(text);
 		if(type === 1){
 			//Up
-			this.state.currentUp = parseInt(text);
+			this.setState({
+				HighPrice: text,
+				upError: value < this.state.stockPriceAsk ? "低于当前价" : null
+			});
 		}else{
 			//Down
-			this.state.currentDown = parseInt(text);
+			this.setState({
+				LowPrice: text,
+				downError: value > this.state.stockPriceBid ? "高于当前价" : null
+			});
 		}
 	},
 
@@ -136,17 +189,34 @@ var EditAlertPage = React.createClass({
 	renderAlertCell: function(type){
 		//type 1:买涨， 2:买跌
 		var title = type === 1 ? '买涨价格高于':'买跌价格低于'
+		var textColor = "black";
+		var text = null;
+		var inputEnable = false;
+		if(type === 1){
+			textColor = this.state.upError ? "red" : "black";
+			text = this.state.HighPrice ? this.state.HighPrice.toString() : null;
+			inputEnable = this.state.HighEnabled;
+		}else{
+			textColor = this.state.downError ? "red" : "black";
+			text = this.state.LowPrice ? this.state.LowPrice.toString() : null;
+			inputEnable = this.state.LowEnabled;
+
+		}
+
 		return(
 			<View style={styles.cellWrapper}>
 				<Text style={styles.cellTitle}>
 					{title}
 				</Text>
-				<TextInput style={styles.cellInput} onChangeText={(text) => this.updatePrice(type, text)}
+				<TextInput style={[styles.cellInput, {color: textColor}]}
+				 					 onChangeText={(text) => this.updatePrice(type, text)}
+									 value={text}
+									 editable={inputEnable}
 									 keyboardType='numeric'>
 				</TextInput>
 				<Switch
-				  value={type===1 ? this.state.switchHigh : this.state.switchLow}
-					onValueChange={(value) => this.setState(type===1 ? {switchHigh:value} : {switchLow:value})}
+				  value={type===1 ? this.state.HighEnabled : this.state.LowEnabled}
+					onValueChange={(value) => this.setState(type === 1 ? {HighEnabled:value} : {LowEnabled:value})}
 				/>
 			</View>
 			)
@@ -155,12 +225,22 @@ var EditAlertPage = React.createClass({
 	onComplete: function(){
 		var userData = LogicData.getUserData();
 
+		var highPrice = null;
+		if(this.state.HighPrice){
+			highPrice = parseFloat(this.state.HighPrice);
+		}
+
+		var lowPrice = null;
+		if(this.state.LowPrice){
+			lowPrice = parseFloat(this.state.LowPrice);
+		}
+
 		var alertData = {
     	"SecurityId": this.props.stockId,
-    	"HighPrice": this.state.currentUp,
-    	"HighEnabled": this.state.switchHigh,
-    	"LowPrice": this.state.currentDwon,
-    	"LowEnabled": this.state.switchLow
+    	"HighPrice": highPrice,
+    	"HighEnabled": this.state.HighEnabled,
+    	"LowPrice": lowPrice,
+    	"LowEnabled": this.state.LowEnabled
 		}
 
 		NetworkModule.fetchTHUrl(
@@ -184,6 +264,16 @@ var EditAlertPage = React.createClass({
 		)
 	},
 
+	hasError: function(){
+		if(this.state.LowEnabled && !this.state.LowPrice){
+			return true;
+		}
+		if(this.state.HighEnabled && !this.state.HighPrice){
+			return true;
+		}
+		return this.state.upError || this.state.downError ? true : false;
+	},
+
 	render: function() {
 		return (
 			<View style={styles.wrapper}>
@@ -191,10 +281,11 @@ var EditAlertPage = React.createClass({
 					showBackButton={true}
 					textOnRight='完成'
 					rightTextOnClick={this.onComplete}
+					enableRightText={!this.hasError()}
 					navigator={this.props.navigator}/>
 				<View style={styles.headerView}>
 					<Text style={styles.nameText}>
-						{this.props.stockId}
+						{this.props.stockInfo.name}
 					</Text>
 					<Text style={styles.priceText}>
 						当前买涨价格 {this.state.stockPriceAsk} 当前买跌价格 {this.state.stockPriceBid}

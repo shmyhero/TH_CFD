6023a1194fbba1304914ab153a9080112f9a6632
Neko <radishjr@gmail.com>
2016-09-07 14:02:19 +0800

    Update webpage version to 1.5.

--- js/VersionConstants.js
+++ js/VersionConstants.js
@@ -1,8 +1,13 @@
 'use strict'
 
 export const WEBVIEW_QA_VERSION = '1.4' //Only update this version when QA Page version is updated!
+export const WEBPAGE_VERSION = '1.5'    //Only update this version when Web Page version is updated!
+export const WEBPAGE_FOLDER = 'TH_CFD_WEB' + WEBPAGE_VERSION
 
-export const WEBPAGE_RELEASE_VERSION = 'TH_CFD_WEB'
-export const WEBPAGE_TEST_VERSION = 'TH_CFD_WEBTEST'
-
-export const WEBPAGE_FOLDER = WEBPAGE_TEST_VERSION  //UPDATE the folder to TH_CFD_WEB in release version!
+//TODO: There will be product and development environment  in later version..
+//export const IS_DEVELOPMENT_ENVIRONMENT = true
+//export const WEBPAGE_PRODUCT_VERSION = 'TH_CFD_WEB'
+//export const WEBPAGE_DEVELOP_VERSION = 'TH_CFD_WEBTEST'
+//
+//export const WEBPAGE_FOLDER = IS_DEVELOPMENT_ENVIRONMENT ? WEBPAGE_DEVELOP_VERSION : WEBPAGE_PRODUCT_VERSION
+////UPDATE the folder to TH_CFD_WEB in release version!

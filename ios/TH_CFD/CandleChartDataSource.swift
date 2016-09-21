42513eaecbd2488007871969d8cd04395468c8ba
nuqiewuoz <william@w159582.dow.com>
2016-09-21 16:59:50 +0800

    update iOS chart view.

---
+++ ios/TH_CFD/CandleChartDataSource.swift
@@ -0,0 +1,11 @@
+//
+//  CandleChartDataSource.swift
+//  TH_CFD
+//
+//  Created by william on 16/9/21.
+//  Copyright © 2016年 Facebook. All rights reserved.
+//
+
+class CandleChartDataSource: BaseDataSource {
+
+}

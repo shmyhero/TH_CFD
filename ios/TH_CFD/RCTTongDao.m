8b1837bb90ab951e8b7e0113a994b0e22031f506
nuqiewuoz <william@w159582.dow.com>
2016-09-29 14:29:02 +0800

    update tongdao sdk.

--- ios/TH_CFD/RCTTongDao.m
+++ ios/TH_CFD/RCTTongDao.m
@@ -7,7 +7,24 @@
 //
 
 #import "RCTTongDao.h"
+#import <TongDaoUILibrary/TongDaoUiCore.h>
 
 @implementation RCTTongDao
 
+RCT_EXPORT_MODULE(TongDaoAPI)
+
+RCT_EXPORT_METHOD(trackUserProfile:(NSDictionary *)profile)
+{
+	[[TongDaoUiCore sharedManager] identify:profile];
+}
+
+RCT_EXPORT_METHOD(trackEvent:(NSString *)eventName value:(NSDictionary *)values)
+{
+	if (values == nil) {
+		[[TongDaoUiCore sharedManager]trackWithEventName:eventName];
+	}
+	else {
+		[[TongDaoUiCore sharedManager] trackWithEventName:eventName andValues:values];
+	}
+}
 @end

b38348ac58ed3275403b76b1618ded302993e2e3
Neko <radishjr@gmail.com>
2017-03-23 10:05:00 +0800

    Remove test code.

--- js/view/MePage.js
+++ js/view/MePage.js
@@ -548,8 +548,7 @@ var MePage = React.createClass({
 		var meData = LogicData.getMeData();
 		console.log('提示：','liveAccStatus = '+meData.liveAccStatus + ', liveAccRejReason = '+ meData.liveAccRejReason)
 	  accStatus = meData.liveAccStatus;
-		accStatus = 0
-		this.state.lastStep = 6
+		//accStatus = 0
 		var strStatus = '';
 		var colorStatus = ColorConstants.TITLE_BLUE
 

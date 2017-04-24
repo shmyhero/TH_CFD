7a73b6f0d0499fcd665b52eb3fb3c31912af8b40
Neko <radishjr@gmail.com>
2017-04-24 17:32:56 +0800

    Change API url

--- js/NetConstants.js
+++ js/NetConstants.js
@@ -163,8 +163,8 @@ function getCFDAPI(){
 
     SHOW_USER_DATA_API: CFD_API_SERVER + '/api/user/live/profit',
 
-    PERSONAL_PAGE_POSITION_OPEN: CFD_API_SERVER + '/api/position/live/open/<userID>',
-    PERSONAL_PAGE_POSITION_CLOSE: CFD_API_SERVER + '/api/position/live/closed/<userID>',
+    PERSONAL_PAGE_POSITION_OPEN: CFD_API_SERVER + '/api/<userID>/position/live/open/',
+    PERSONAL_PAGE_POSITION_CLOSE: CFD_API_SERVER + '/api/<userID>/position/live/closed/',
   }
 }
 
